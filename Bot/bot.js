3b3e41b791eb0e79679d1a74978cf736c5a72578
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2020-05-13 19:55:20 +0100

    Basic ping pong stuff working

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,15 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
+const prefix = '!';
+
+// Queue bot vars.
+
+var PlayerArray = new Array();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if(!msg.content.startsWith(prefix) || msg.author.bot) return;               // Ignore all commands that arent commands.
+
+  const args = msg.content.slice(prefix.length).split(' ');                   // Split the commands and args.
+  const command = args.shift().toLowerCase();
+
+  printDebug(args, command, msg);                                             // Used to debug can be disabled later.
+
+  if(command == 'queue'){
+    addPlayerToQueue(command);
   }
+
 });
 
 client.login(auth.token);
+
+function addPlayerToQueue(commandArg){
+  
+}
+
+function printDebug(args, command, messageDebug){
+  if(command == 'args'){
+    messageDebug.channel.send('Arguments Provided: ' + args + ' ' + messageDebug.author + '!');
+  }
+
+  if(command == 'ping'){
+    messageDebug.reply('pong');
+  }
+}
