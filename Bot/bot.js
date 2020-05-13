fa389a3424c5957ed60b810ac73b8243ff47de1b
Luke Whiting <43708922+lukewhitingdev@users.noreply.github.com>
2020-05-14 00:49:23 +0100

    Added basic functionality

--- Bot/bot.js
+++ Bot/bot.js
@@ -23,6 +23,12 @@ client.on('message', msg => {
   if(command == 'queue'){
     addPlayerToQueue(command, msg);
   }
+  if(command == 'leave'){
+    removePlayerFromQueue(msg);
+  }
+  if(command == 'print'){
+    printQueue(msg);
+  }
   if(command == 'clear'){
     clearPlayerQueue(msg);
   }
@@ -30,6 +36,8 @@ client.on('message', msg => {
 });
 
 client.login(auth.token);
+
+// Autonimous game functions.
 
 function popQueue(messageDebug){
   var stringConcat = "";
@@ -40,23 +48,33 @@ function popQueue(messageDebug){
   messageDebug.channel.send(stringConcat);
 }
 
+// Command functions
+
 function addPlayerToQueue(commandArg, messageDebug){
-  var messageUsername = messageDebug.author.username;
 
   messageDebug.reply('Added to queue!');
   PlayerArray.push(messageDebug.author);
-  messageUsername = messageUsername + "\n";
-
-  if(PlayerQueueString == ""){
-    PlayerQueueString = messageUsername;
-  }else{
-    PlayerQueueString = PlayerQueueString + messageUsername;
-  }
   printQueue(messageDebug);
 
   if(PlayerArray.length == matchSize){
     popQueue(messageDebug);
   }
+}
+
+function removePlayerFromQueue(messageDebug){
+  var deleteIndex = 999;
+
+  for(var i = 0; i < PlayerArray.length; i++){
+    if(PlayerArray[i] == messageDebug.author){              // If we find the player then record the index and stop the loop.
+      deleteIndex = i;
+      break;
+    }
+  }
+
+  if(deleteIndex != 999){
+    delete PlayerArray[deleteIndex];                      // If we have found the player then delete them from the array.
+  }
+  printQueue(messageDebug);
 }
 
 function clearPlayerQueue(messageDebug){
@@ -67,7 +85,22 @@ function clearPlayerQueue(messageDebug){
 }
 
 function printQueue(message){
+  rebuildPlayerListString();                                          // make sure we have the most updated player list to be showing.
   message.channel.send('```[Queue]\n' + PlayerQueueString + '```');
+}
+
+// Helper Functions
+
+function rebuildPlayerListString(){
+  PlayerQueueString = "";
+  PlayerArray.forEach(player => {
+    var messageUsername = player.username + "\n";
+    if(PlayerQueueString == ""){
+      PlayerQueueString = messageUsername;
+    }else{
+      PlayerQueueString = PlayerQueueString + messageUsername;
+    }
+  });
 }
 
 function printDebug(args, command, messageDebug){
